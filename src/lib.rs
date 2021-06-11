7147930c923d8cab3bdea52cbadd70832bd54998
gcxfd <i@rmw.link>
2021-06-11 10:27:31 +0800

    🔶

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 pub enum SdbArgs<'a> {
   Dir(&'a PathBuf),
   Filename(&'a str),
-  InitDbSize(u64),
+  InitSize(u64),
   MaxTx(usize),
 }
 
@@ -71,7 +71,7 @@ impl Sdb {
   pub fn new(args: &[SdbArgs]) -> Sdb {
     let mut dir = None;
     let mut filename = None;
-    let mut init_db_size = None;
+    let mut init_size = None;
     let mut max_tx = None;
     use SdbArgs::*;
 
@@ -79,17 +79,17 @@ impl Sdb {
       match arg {
         Dir(i) => dir = (*i).into(),
         Filename(i) => filename = i.to_string().into(),
-        InitDbSize(i) => init_db_size = (*i).into(),
+        InitSize(i) => init_size = (*i).into(),
         MaxTx(i) => max_tx = (*i).into(),
       }
     }
 
     let dir = dir.unwrap();
     let filename = filename.unwrap_or_else(|| "sdb".into());
-    let init_db_size = init_db_size.unwrap_or(1 << 21);
+    let init_size = init_size.unwrap_or(1 << 21);
     let max_tx = max_tx.unwrap_or(3);
 
     create_dir_all(&dir).unwrap();
-    Sdb(Env::new(dir.join(filename), init_db_size, max_tx).unwrap())
+    Sdb(Env::new(dir.join(filename), init_size, max_tx).unwrap())
   }
 }
