1aef1102dee96ec0c471c9adfe584e3dd23c4e3b
gcxfd <i@rmw.link>
2021-06-11 10:12:51 +0800

    🔶

--- tests/main.rs
+++ tests/main.rs
@@ -25,9 +25,11 @@ fn main() -> Result<()> {
   let db = sdb.db::<u64, u64>(0);
   let mut w = db.w()?;
   println!("db.id {}", db.id);
-  btree::put(&mut w.tx, &mut w.tree, &1, &8)?;
-  btree::put(&mut w.tx, &mut w.tree, &5, &10)?;
+  btree::put(&mut w.tx, &mut w.tree, &5, &11)?;
+  btree::put(&mut w.tx, &mut w.tree, &5, &12)?;
   w.commit()?;
+  let mut w = db.w()?;
+  btree::put(&mut w.tx, &mut w.tree, &5, &13)?;
 
   let r = db.r()?;
   for entry in btree::iter(&r.tx, r.tree, None)? {
