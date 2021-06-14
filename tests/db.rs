19a58be2f25a3d715bb4318a88ee82bb5ba0b874
gcxfd <i@rmw.link>
2021-06-14 20:41:58 +0800

    🔶

---
+++ tests/db.rs
@@ -0,0 +1,35 @@
+use sdb::{Db, Tx};
+use static_init::dynamic;
+use std::env;
+use std::path::Path;
+
+#[dynamic]
+pub static DIR: String = env::current_exe()
+  .unwrap()
+  .parent()
+  .unwrap()
+  .parent()
+  .unwrap()
+  .display()
+  .to_string();
+
+#[dynamic]
+pub static TX: Tx = {
+  let dir = Path::new(&*DIR).join("db");
+
+  println!("DATABASE DIR {}", dir.display().to_string());
+
+  //use sdb::SdbArgs::{InitSize, MaxTx, Filename};
+
+  Tx::new(
+    &dir,
+    &[
+      //MaxTx(3),
+      //Filename("sdb"),
+      //InitSize(1<<21),
+    ],
+  )
+};
+
+#[dynamic]
+pub static DB0: Db<'static, u64, u64> = TX.db(0);
--- tests/main.rs
+++ tests/main.rs
@@ -1,37 +1,7 @@
-use sdb::{Db, Tx};
-use static_init::dynamic;
-use std::env;
-use std::path::Path;
-
-#[dynamic]
-pub static DIR: String = env::current_exe()
-  .unwrap()
-  .parent()
-  .unwrap()
-  .parent()
-  .unwrap()
-  .display()
-  .to_string();
-
-#[dynamic]
-pub static TX: Tx = {
-  let dir = Path::new(&*DIR).join("db");
-
-  println!("DATABASE DIR {}", dir.display().to_string());
-
-  //use sdb::SdbArgs::{InitSize, MaxTx, Filename};
-
-  Tx::new(
-    &dir,
-    &[
-      //MaxTx(3),
-      //Filename("sdb"),
-      //InitSize(1<<21),
-    ],
-  )
-};
-
+mod db;
 use anyhow::Result;
+use db::TX;
+use sdb::Db;
 
 #[test]
 fn main() -> Result<()> {
@@ -48,6 +18,7 @@ fn main() -> Result<()> {
     let (k, v) = entry?;
     println!("> {:?} {:?}", k, v)
   }
+
   //tx.commit()?;
 
   //let t2 = tx.db(&*T2)?;
